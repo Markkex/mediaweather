ab771ba106327665a19bdd07f8c7a1d762b8d574
Markkex <marco.avila.93@hotmail.com>
2021-06-15 15:03:30 +0100

    Styling comments component

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -1,17 +1,30 @@
-import React from 'react'
-
-const Comments = ({users, setUsers}) => {
-    console.log(users)
-    return (
-        <div> 
-          {users.map((user) => (
-              <div>
-                  <p>{user.username}</p>
-                  <p>{user.comment}</p>
-              </div>
-          ))}
+import React from "react";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
+import "./Comments.css"
+const Comments = ({ users, setUsers }) => {
+  console.log(users);
+  return (
+    <div>
+      {users.map((user) => (
+        <div className='comments'>
+          <div>
+            <p>{user.username}</p>
+            <p>{user.comment}</p>
+          </div>
+          <div className="edit-options">
+            <Button>
+              <EditIcon />
+            </Button>
+            <Button>
+              <DeleteIcon />
+            </Button>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Comments
+export default Comments;
