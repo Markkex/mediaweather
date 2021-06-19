69a18c736f0bd8a2c3a09de298a1272d0710daff
Markkex <marco.avila.93@hotmail.com>
2021-06-19 19:23:58 +0100

    Refactor to Weather card to App Level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import WeatherCard from "./Components/WeatherCard";
+/*CSS*/
+
+/*Components*/
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import Comments from "./Components/Comments";
+import Form from "./Components/Form";
 
 function App() {
   const [count, setCount] = useState(0);
   const [weather, setWeather] = useState(undefined);
   const [input, setInput] = useState("");
+
+
   const [users, setUsers] = useState([
     { id: 1, username: "RickRoss96", comment: "The weather is great!" },
     {
@@ -31,20 +43,51 @@ function App() {
         setWeather(result);
       });
   };
-
+  const counterIncrease = () => {
+    setCount(count + 1);
+  };
   return (
     <div className='App'>
       {weather && (
-        <WeatherCard
-          count={count}
-          data={weather}
-          setCount={setCount}
-          setUsers={setUsers}
-          users={users}
-          input={input}
-          setInput={setInput}
-        />
-      )}
+            <Card className="weathercard">
+            <CardHeader
+              avatar={
+                <Avatar aria-label='weatheravatar'>
+                  <img src="https://nordicapis.com/wp-content/uploads/5-Best-Free-and-Paid-Weather-APIs-2019-e1587582023501.png" alt='Weather' />
+                </Avatar>
+              }
+              title='Media Weather'
+              subheader={`Displaying weather in ${weather.name}. But it could be on Mars.`}
+            />
+            <CardContent>
+              <div className='weather-content'>
+                <div className='weather-text'>
+                  <p>{weather.main.temp}</p>
+                  <p>{weather.weather[0].description}</p>
+                </div>
+              </div>
+              {users.map((user) => (
+                <Comments
+                  key={user.id}
+                  user={user}
+                  users={users}
+                  setUsers={setUsers}
+                />
+              ))}
+            </CardContent>
+            <CardActions className='cardactions'>
+              <div className='likebutton'>
+                <FavoriteIcon
+                  className='favorite-icon-color'
+                  onClick={counterIncrease}
+                />
+                {count}
+              </div>
+    
+              <Form setUsers={setUsers} users={users} input={input} setInput={setInput} />
+            </CardActions>
+          </Card>
+        )}
     </div>
   );
 }
